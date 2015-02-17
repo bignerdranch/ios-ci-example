1d197ea7650dc23d4843bce9a9cdd23ea05bff48
Florian Harr <florian@bignerdranch.com>
2015-02-17 12:01:36 -0500

    Initial commit

---
+++ ContinuousDistributionExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ContinuousDistributionExample
+//
+//  Created by Florian Harr on 17/02/15.
+//  Copyright (c) 2015 Big Nerd Ranch, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ContinuousDistributionExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ContinuousDistributionExample
+//
+//  Created by Florian Harr on 17/02/15.
+//  Copyright (c) 2015 Big Nerd Ranch, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
